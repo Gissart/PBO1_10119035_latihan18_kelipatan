aaf1b6364b98009ad3a3af319b1035ea82237665
unknown <argya.programmer@gmail.com>
2020-10-31 12:04:35 +0700

    Initial Commit

---
+++ src/com/tugasPBO/kelipatan.java
@@ -0,0 +1,15 @@
+package com.tugasPBO;
+
+//nama = Argya Aulia Fauzandika
+//nim = 10119035
+//kelas = IF1
+//Deskripsi = menampilkan kelipatan 3.5 menggunakan perulangan for
+public class kelipatan {
+    public static void main(String[] args) {
+        double bil = 0;
+        for (int i = 0; i < 10; i++) {
+            bil += 3.5;
+            System.out.println(bil);
+        }
+    }
+}
